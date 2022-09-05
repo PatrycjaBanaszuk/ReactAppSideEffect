dddac2494882b285cb0e0a141e2f5085ebfe49ee
Patrycja Banaszuk <pwbanaszuk@gmail.com>
2022-09-05 18:00:20 +0100

    useContext

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useReducer, useEffect } from 'react'
+import React, { useState, useReducer, useEffect, useContext} from 'react'
 
 import Card from '../UI/Card/Card'
 import classes from './Login.module.css'
 import Button from '../UI/Button/Button'
+import AuthContext from '../../store/auth-context'
 
 const emailReducer = (state, action) => {
 	if (action.type === 'USER_INPUT') {
@@ -41,6 +42,8 @@ const Login = props => {
 		isValid: false,
 	})
 
+	const AuthCtx = useContext(AuthContext)
+
 const { isValid: emailIsValid } = emailState;
 const { isValid: passwordIsValid } = passwordState;
 
@@ -77,7 +80,7 @@ const { isValid: passwordIsValid } = passwordState;
 
 	const submitHandler = event => {
 		event.preventDefault()
-		props.onLogin(emailState.value, passwordState.value)
+		AuthCtx.onLogin(emailState.value, passwordState.value)
 	}
 
 	return (
